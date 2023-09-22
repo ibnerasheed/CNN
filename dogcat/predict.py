9e64f39394eebe643c4cf5457113959d36b171b4
ibnerasheed <ibnerasheed@gmail.com>
2023-09-22 22:36:05 +0530

    new changes

--- dogcat/predict.py
+++ dogcat/predict.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 20 18:45:05 2020
+Created on Fri 22 sept 2023
 
-@author: sudhanshukumar
+@author: adil rasheed
 """
 
 import numpy as np
